448b373f495ef5911e653ba2c8511da455a1ccae
Danil Sadovoy <53143990+Sadovoy-Danil@users.noreply.github.com>
2019-07-26 13:11:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var number = 5; // — число
+var string = "Hello!"; // — строка
+var sym = Symbol(); // — символ
+var boolean = true; // — булиновое значение
+null;
+undefined;
+var obj = {}; // — объект
+
+console.log(4/0); // infinity
+console.log(5 / "hello"); // NaN
+
+console.log(something); // null
+console.log(human); // undefined
+
+var human = "My name is Danil";
+
+let persone = {
+       name: 'John',
+       age: 25,         // ключ: значение
+       isMarried: false
+};
+
+console.log(persone.name); // or console.log(persone["name"]); 
+
+let arr = ['plum.png', 'orange.jpg', 'apple.jpg']; // массив
+
+console.log(arr[2]);
